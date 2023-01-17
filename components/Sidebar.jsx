83575d554f6d93e92e5fa109b22604166245bcfb
guilhermemm-dev <guilhermemm.dev@gmail.com>
2023-01-17 12:36:05 +0000

    minor fixes

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -11,34 +11,34 @@ const Sidebar = ({ children }) => {
       <div className='fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between'>
         <div className='flex flex-col items-center'>
           <Link href='/'>
-            <div className='bg-purple-800 text-white p-3 rounded-lg inline-block'>
+            <div className='inline-block p-3 text-white bg-purple-800 rounded-lg'>
               <RxSketchLogo size={20} />
             </div>
           </Link>
           <span className='border-b-[1px] border-gray-200 w-full p-2'></span>
           <Link href='/'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+            <div className='inline-block p-3 my-4 bg-gray-100 rounded-lg cursor-pointer hover:bg-gray-200'>
               <RxDashboard size={20} />
             </div>
           </Link>
           <Link href='/customers'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+            <div className='inline-block p-3 my-4 bg-gray-100 rounded-lg cursor-pointer hover:bg-gray-200'>
               <RxPerson size={20} />
             </div>
           </Link>
           <Link href='/orders'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+            <div className='inline-block p-3 my-4 bg-gray-100 rounded-lg cursor-pointer hover:bg-gray-200'>
               <HiOutlineShoppingBag size={20} />
             </div>
           </Link>
           <Link href='/'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+            <div className='inline-block p-3 my-4 bg-gray-100 rounded-lg cursor-pointer hover:bg-gray-200'>
               <FiSettings size={20} />
             </div>
           </Link>
         </div>
       </div>
-      <main className='ml-20 w-full'>{children}</main>
+      <main className='w-full ml-20'>{children}</main>
     </div>
   );
 };
